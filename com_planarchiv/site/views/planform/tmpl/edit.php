9fa29dfdb65fb6f2d14088356e8a6fe69eb65b64
Thomas Hunziker <github@bakual.ch>
2024-01-18 11:03:28 +0100

    Adjusting form to J5 design

--- com_planarchiv/site/views/planform/tmpl/edit.php
+++ com_planarchiv/site/views/planform/tmpl/edit.php
@@ -48,10 +48,9 @@ $this->useCoreUI        = true;
 
 	<form
 			action="<?php echo Route::_('index.php?option=com_planarchiv&view=planform&id=' . (int) $this->item->id); ?>"
-			method="post" name="adminForm" id="adminForm" class="form-validate form form-vertical">
+			method="post" name="adminForm" id="adminForm" class="form-validate form-vertical">
 		<fieldset>
 			<?php echo $this->form->renderField('title'); ?>
-
 			<?php echo HTMLHelper::_('uitab.startTabSet', $this->tab_name, ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
 
 			<?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'location', Text::_('COM_PLANARCHIV_TAB_LOCATION')); ?>
@@ -86,44 +85,48 @@ $this->useCoreUI        = true;
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
 			<?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'basic', Text::_('COM_PLANARCHIV_TAB_BASIC')); ?>
-			<div class="row-fluid">
-				<div class="pull-left rightMargin">
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderField('ErstellDatum'); ?>
 				</div>
-				<div class="pull-left">
+				<div class="col-md-3">
 					<?php echo $this->form->renderField('ersteller_id'); ?>
 				</div>
 			</div>
-			<div class="row-fluid">
-				<div class="pull-left rightMargin">
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderField('AenderungsDatum'); ?>
 				</div>
-				<div class="pull-left">
+				<div class="col-md-1">
 					<?php echo $this->form->renderField('Index1'); ?>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-6">
 			<?php echo $this->form->renderField('CAD_Auftrag'); ?>
-			<div class="row-fluid">
-				<div class="pull-left rightMargin">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderField('anlagetyp_id'); ?>
 				</div>
-				<div class="pull-left">
+				<div class="col-md-1">
 					<?php echo $this->form->renderField('AnlageLfnr'); ?>
 				</div>
 			</div>
-			<div class="row-fluid">
-				<div class="pull-left rightMargin">
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderField('dokutyp_id'); ?>
 				</div>
-				<div class="pull-left">
+				<div class="col-md-1">
 					<?php echo $this->form->renderField('DokuTypNr'); ?>
 				</div>
 			</div>
-			<div class="row-fluid">
-				<div class="pull-left rightMargin">
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderField('zurzeitbei_id'); ?>
 				</div>
-				<div class="pull-left">
+				<div class="col-md-3">
 					<?php echo $this->form->renderField('zurzeitbei_date'); ?>
 				</div>
 			</div>
@@ -136,11 +139,11 @@ $this->useCoreUI        = true;
 			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
 			<?php echo HTMLHelper::_('uitab.addTab', $this->tab_name, 'details', Text::_('COM_PLANARCHIV_TAB_DETAILS')); ?>
-			<div class="row-fluid">
-				<div class="span6">
+			<div class="row">
+				<div class="col-md-6">
 					<?php echo $this->form->renderFieldset('info'); ?>
 				</div>
-				<div class="span6">
+				<div class="col-md-6">
 					<?php echo $this->form->renderFieldset('global'); ?>
 				</div>
 			</div>
@@ -152,33 +155,23 @@ $this->useCoreUI        = true;
 			<input type="hidden" name="return" value="<?php echo $this->return_page; ?>"/>
 			<?php echo HTMLHelper::_('form.token'); ?>
 		</fieldset>
-		<div class="btn-toolbar">
-			<div class="btn-group">
-				<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('plan.save')">
-					<i class="icon-ok"></i> <?php echo Text::_('JSAVE') ?>
-				</button>
-			</div>
-			<div class="btn-group">
-				<button type="button" class="btn" onclick="Joomla.submitbutton('plan.save2copy')">
-					<i class="icon-save-copy"></i> <?php echo Text::_('COM_PLANARCHIV_SAVE_AS_COPY') ?>
-				</button>
-			</div>
-			<div class="btn-group">
-				<button type="button" class="btn" onclick="Joomla.submitbutton('plan.cancel')">
-					<i class="icon-cancel"></i> <?php echo Text::_('JCANCEL') ?>
-				</button>
-			</div>
+		<div class="mb-2">
+			<button type="button" class="btn btn-primary" data-submit-task="plan.save">
+				<span class="icon-ok"></span> <?php echo Text::_('JSAVE') ?>
+			</button>
+			<button type="button" class="btn btn-primary" data-submit-task="plan.save2copy">
+				<span class="icon-save-copy"></span> <?php echo Text::_('COM_PLANARCHIV_SAVE_AS_COPY') ?>
+			</button>
+			<button type="button" class="btn btn-danger" data-submit-task="plan.cancel">
+				<span	 class="icon-cancel"></span> <?php echo Text::_('JCANCEL') ?>
+			</button>
 			<?php if ($this->item->id) : ?>
-				<div class="btn-group">
-					<?php echo $this->form->getInput('contenthistory'); ?>
-				</div>
+				<?php echo $this->form->getInput('contenthistory'); ?>
 				<?php if (Factory::getApplication()->getIdentity()->authorise('core.delete', 'com_planarchiv.category.' . $this->item->catid)) : ?>
-					<div class="btn-group">
-						<button type="button" class="btn btn-danger"
-								onclick="if(confirm('<?php echo Text::_('COM_PLANARCHIV_CONFIRM_DELETE'); ?>')) {document.getElementById('jform_state').value='-2';Joomla.submitbutton('plan.save');} else {return;}">
-							<i class="icon-delete"></i> <?php echo Text::_('JACTION_DELETE') ?>
-						</button>
-					</div>
+					<button type="button" class="btn btn-danger"
+							onclick="if(confirm('<?php echo Text::_('COM_PLANARCHIV_CONFIRM_DELETE'); ?>')) {document.getElementById('jform_state').value='-2';Joomla.submitbutton('plan.save');} else {return;}">
+						<span class="icon-delete"></span> <?php echo Text::_('JACTION_DELETE') ?>
+					</button>
 				<?php endif; ?>
 			<?php endif; ?>
 		</div>
