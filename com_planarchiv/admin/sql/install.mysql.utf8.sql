fce1df27a7d7855e83c72ccba25df506005287f6
Thomas Hunziker <github@bakual.ch>
2024-01-24 15:26:03 +0100

    Create the content types during install/update

--- com_planarchiv/admin/sql/install.mysql.utf8.sql
+++ com_planarchiv/admin/sql/install.mysql.utf8.sql
@@ -5,136 +5,179 @@ DROP TABLE IF EXISTS `#__planarchiv_didok`;
 DROP TABLE IF EXISTS `#__planarchiv_dokutyp`;
 DROP TABLE IF EXISTS `#__planarchiv_stockwerk`;
 
-CREATE TABLE `#__planarchiv_plan` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `ErstellDatum` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `ersteller_id` int(11) NOT NULL,
-  `PlanErsteller` varchar(255) DEFAULT NULL,
-  `Index1` varchar(1) DEFAULT NULL,
-  `AenderungsDatum` datetime DEFAULT '0000-00-00 00:00:00',
-  `CAD_Auftrag` varchar(30) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL COMMENT 'AKS Code',
-  `Maengelliste` tinyint(1) DEFAULT '0',
-  `aktualisiert` tinyint(1) DEFAULT '0',
-  `Bemerkung` longtext,
-  `didok_id` int(10) NOT NULL,
-  `stockwerk_id` int(10) NOT NULL,
-  `dfa_id` int(10) NOT NULL,
-  `GebDfaLfnr` varchar(255) DEFAULT NULL,
-  `Strecke` varchar(40) DEFAULT NULL,
-  `km` double DEFAULT NULL,
-  `richtung_didok_id` int(10) NOT NULL,
-  `anlagetyp_id` int(10) NOT NULL,
-  `AnlageLfnr` varchar(255) DEFAULT NULL,
-  `DokuTypNr` varchar(255) DEFAULT NULL,
-  `dokutyp_id` int(11) DEFAULT NULL,
-  `zurzeitbei_id` int(10) NOT NULL,
-  `zurzeitbei_date` datetime DEFAULT NULL,
-  `files` varchar(50) NOT NULL,
-  `original` tinyint(1) NOT NULL,
-  `alignment` varchar(1) NOT NULL,
-  `size` varchar(50) NOT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  `language` char(7) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_plan`
+(
+    `id`                int(10)     NOT NULL AUTO_INCREMENT,
+    `ErstellDatum`      datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `ersteller_id`      int(11)     NOT NULL,
+    `Index1`            varchar(1)           DEFAULT NULL,
+    `AenderungsDatum`   datetime             DEFAULT '0000-00-00 00:00:00',
+    `CAD_Auftrag`       varchar(30)          DEFAULT NULL,
+    `title`             varchar(255)         DEFAULT NULL COMMENT 'AKS Code',
+    `Maengelliste`      tinyint(1)           DEFAULT '0',
+    `aktualisiert`      tinyint(1)           DEFAULT '0',
+    `Bemerkung`         longtext,
+    `didok_id`          int(10)     NOT NULL,
+    `stockwerk_id`      int(10)     NOT NULL,
+    `dfa_id`            int(10)     NOT NULL,
+    `GebDfaLfnr`        varchar(255)         DEFAULT NULL,
+    `Strecke`           varchar(40)          DEFAULT NULL,
+    `km`                double               DEFAULT NULL,
+    `richtung_didok_id` int(10)     NOT NULL,
+    `anlagetyp_id`      int(10)     NOT NULL,
+    `AnlageLfnr`        varchar(255)         DEFAULT NULL,
+    `DokuTypNr`         varchar(255)         DEFAULT NULL,
+    `dokutyp_id`        int(11)              DEFAULT NULL,
+    `zurzeitbei_id`     int(10)     NOT NULL,
+    `zurzeitbei_date`   datetime             DEFAULT NULL,
+    `files`             varchar(50) NOT NULL,
+    `original`          tinyint(1)  NOT NULL,
+    `alignment`         varchar(1)  NOT NULL,
+    `size`              varchar(50) NOT NULL,
+    `catid`             int(10)     NOT NULL,
+    `state`             tinyint(3)  NOT NULL,
+    `created`           datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`        int(10)     NOT NULL DEFAULT '0',
+    `modified`          datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`       int(10)     NOT NULL DEFAULT '0',
+    `checked_out`       int(10)     NOT NULL DEFAULT '0',
+    `checked_out_time`  datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`           int(10)     NOT NULL,
+    `language`          char(7)     NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `#__planarchiv_anlagetyp` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `title_de` varchar(255) DEFAULT NULL,
-  `title_fr` varchar(255) DEFAULT NULL,
-  `title_it` varchar(255) DEFAULT NULL,
-  `code` varchar(255) NOT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_anlagetyp`
+(
+    `id`               int(10)      NOT NULL AUTO_INCREMENT,
+    `title_de`         varchar(255)          DEFAULT NULL,
+    `title_fr`         varchar(255)          DEFAULT NULL,
+    `title_it`         varchar(255)          DEFAULT NULL,
+    `code`             varchar(255) NOT NULL,
+    `catid`            int(10)      NOT NULL,
+    `state`            tinyint(3)   NOT NULL,
+    `created`          datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`       int(10)      NOT NULL DEFAULT '0',
+    `modified`         datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`      int(10)      NOT NULL DEFAULT '0',
+    `checked_out`      int(10)      NOT NULL DEFAULT '0',
+    `checked_out_time` datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`          int(10)      NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `#__planarchiv_dfa` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `code_de` varchar(255) NOT NULL,
-  `title_de` varchar(255) DEFAULT NULL,
-  `code_fr` varchar(255) DEFAULT NULL,
-  `title_fr` varchar(255) DEFAULT NULL,
-  `code_it` varchar(255) DEFAULT NULL,
-  `title_it` varchar(255) DEFAULT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_dfa`
+(
+    `id`               int(10)      NOT NULL AUTO_INCREMENT,
+    `code_de`          varchar(255) NOT NULL,
+    `title_de`         varchar(255)          DEFAULT NULL,
+    `code_fr`          varchar(255)          DEFAULT NULL,
+    `title_fr`         varchar(255)          DEFAULT NULL,
+    `code_it`          varchar(255)          DEFAULT NULL,
+    `title_it`         varchar(255)          DEFAULT NULL,
+    `catid`            int(10)      NOT NULL,
+    `state`            tinyint(3)   NOT NULL,
+    `created`          datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`       int(10)      NOT NULL DEFAULT '0',
+    `modified`         datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`      int(10)      NOT NULL DEFAULT '0',
+    `checked_out`      int(10)      NOT NULL DEFAULT '0',
+    `checked_out_time` datetime     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`          int(10)      NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `#__planarchiv_didok` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
-  `didok` varchar(255) DEFAULT NULL,
-  `ktu` varchar(255) DEFAULT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `didok` (`didok`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_didok`
+(
+    `id`               int(10)    NOT NULL AUTO_INCREMENT,
+    `title`            varchar(255)        DEFAULT NULL,
+    `didok`            varchar(255)        DEFAULT NULL,
+    `ktu`              varchar(255)        DEFAULT NULL,
+    `catid`            int(10)    NOT NULL,
+    `state`            tinyint(3) NOT NULL,
+    `created`          datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`       int(10)    NOT NULL DEFAULT '0',
+    `modified`         datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`      int(10)    NOT NULL DEFAULT '0',
+    `checked_out`      int(10)    NOT NULL DEFAULT '0',
+    `checked_out_time` datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`          int(10)    NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `didok` (`didok`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `#__planarchiv_dokutyp` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `code_de` varchar(255) DEFAULT NULL,
-  `title_de` varchar(255) DEFAULT NULL,
-  `code_fr` varchar(255) DEFAULT NULL,
-  `title_fr` varchar(255) DEFAULT NULL,
-  `code_it` varchar(255) DEFAULT NULL,
-  `title_it` varchar(255) DEFAULT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_dokutyp`
+(
+    `id`               int(10)    NOT NULL AUTO_INCREMENT,
+    `code_de`          varchar(255)        DEFAULT NULL,
+    `title_de`         varchar(255)        DEFAULT NULL,
+    `code_fr`          varchar(255)        DEFAULT NULL,
+    `title_fr`         varchar(255)        DEFAULT NULL,
+    `code_it`          varchar(255)        DEFAULT NULL,
+    `title_it`         varchar(255)        DEFAULT NULL,
+    `catid`            int(10)    NOT NULL,
+    `state`            tinyint(3) NOT NULL,
+    `created`          datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`       int(10)    NOT NULL DEFAULT '0',
+    `modified`         datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`      int(10)    NOT NULL DEFAULT '0',
+    `checked_out`      int(10)    NOT NULL DEFAULT '0',
+    `checked_out_time` datetime   NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`          int(10)    NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
 
-CREATE TABLE `#__planarchiv_stockwerk` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `code` double NOT NULL DEFAULT '0',
-  `title` varchar(50) NOT NULL,
-  `catid` int(10) NOT NULL,
-  `state` tinyint(3) NOT NULL,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `created_by` int(10) NOT NULL DEFAULT '0',
-  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_by` int(10) NOT NULL DEFAULT '0',
-  `checked_out` int(10) NOT NULL DEFAULT '0',
-  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `version` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE `#__planarchiv_stockwerk`
+(
+    `id`               int(10)     NOT NULL AUTO_INCREMENT,
+    `code`             double      NOT NULL DEFAULT '0',
+    `title`            varchar(50) NOT NULL,
+    `catid`            int(10)     NOT NULL,
+    `state`            tinyint(3)  NOT NULL,
+    `created`          datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `created_by`       int(10)     NOT NULL DEFAULT '0',
+    `modified`         datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `modified_by`      int(10)     NOT NULL DEFAULT '0',
+    `checked_out`      int(10)     NOT NULL DEFAULT '0',
+    `checked_out_time` datetime    NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `version`          int(10)     NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
+
+INSERT INTO `#__content_types` (`type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`,
+                                `content_history_options`)
+VALUES ('Planarchiv Plan',
+        'com_planarchiv.plan',
+        '{"special":{"dbtable":"#__planarchiv_plan","key":"id","type":"Plan","prefix":"PlanarchivTable","config":"array()"}}',
+        '',
+        '{
+            "common":{
+                "core_content_item_id":"id",
+                "core_title":"title",
+                "core_state":"state",
+                "core_created_time":"created",
+                "core_modified_time":"modified",
+                "core_body":"bemerkungen",
+                "core_version":"version"
+                "core_catid":"catid"
+            },
+            "special":{}
+        }',
+        'SichtweitenHelperRoute::getLocationRoute',
+        '{
+        "formFile":"components\/com_planarchiv\/models\/forms\/plan.xml",
+        "hideFields":["checked_out","checked_out_time","version"],
+        "ignoreChanges":["checked_out", "checked_out_time", "path"],
+        "convertToInt":[],
+        "displayLookup":[
+               {"sourceColumn":"created_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"modified_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}
+       ]
+   }');
--- com_planarchiv/admin/sql/updates/mysql/2.0.0.sql
+++ com_planarchiv/admin/sql/updates/mysql/2.0.0.sql
@@ -5,6 +5,7 @@ MODIFY `zurzeitbei_id` int NULL,
 MODIFY `ersteller_id` int NULL,
 MODIFY `files` varchar(50) NOT NULL DEFAULT '',
 DROP COLUMN `PlanNr`,
+DROP COLUMN `PlanErsteller`,
 DROP COLUMN `Ort`,
 DROP COLUMN `Stockwerk`,
 DROP COLUMN `GebDfaCode`,
@@ -18,3 +19,43 @@ DROP COLUMN `ZurZeitBei`,
 DROP COLUMN `Seit`,
 DROP COLUMN `Ablage`,
 DROP COLUMN `AblageSeit`;
+
+INSERT INTO `#__content_types` (`type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`,
+                                `content_history_options`)
+VALUES ('Planarchiv Plan',
+        'com_planarchiv.plan',
+        '{"special":{"dbtable":"#__planarchiv_plan","key":"id","type":"Plan","prefix":"PlanarchivTable","config":"array()"}}',
+        '',
+        '{
+            "common":{
+                "core_content_item_id":"id",
+                "core_title":"title",
+                "core_state":"state",
+                "core_created_time":"created",
+                "core_modified_time":"modified",
+                "core_body":"bemerkungen",
+                "core_version":"version"
+                "core_catid":"catid"
+            },
+            "special":{}
+        }',
+        'SichtweitenHelperRoute::getLocationRoute',
+        '{
+        "formFile":"components\/com_planarchiv\/models\/forms\/plan.xml",
+        "hideFields":["checked_out","checked_out_time","version"],
+        "ignoreChanges":["checked_out", "checked_out_time", "path"],
+        "convertToInt":[],
+        "displayLookup":[
+               {"sourceColumn":"anlagetyp_id","targetTable":"#__planarchiv_anlagetyp","targetColumn":"id","displayColumn":"title_de"},
+               {"sourceColumn":"dfa_id","targetTable":"#__planarchiv_dfa","targetColumn":"id","displayColumn":"title_de"},
+               {"sourceColumn":"didok_id","targetTable":"#__planarchiv_didok","targetColumn":"id","displayColumn":"title"},
+               {"sourceColumn":"richtung_didok_id","targetTable":"#__planarchiv_didok","targetColumn":"id","displayColumn":"title"},
+               {"sourceColumn":"dokutyp_id","targetTable":"#__planarchiv_dokutyp","targetColumn":"id","displayColumn":"title_de"},
+               {"sourceColumn":"stockwerk_id","targetTable":"#__planarchiv_stockwerk","targetColumn":"id","displayColumn":"title"},
+               {"sourceColumn":"ersteller_id","targetTable":"#__contact_details","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"zurzeitbei_id","targetTable":"#__contact_details","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"created_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"modified_by","targetTable":"#__users","targetColumn":"id","displayColumn":"name"},
+               {"sourceColumn":"catid","targetTable":"#__categories","targetColumn":"id","displayColumn":"title"}
+       ]
+   }');
